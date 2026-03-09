96081f3cbe92cd2ebea9743ce47a8bf81395a06d
gauthamKrishnan30 <gautham2004k@gmail.com>
2026-03-09 22:26:45 +0530

    CSV File | Preprocess_data| Streamlit

--- preprocessing_dataset.py
+++ preprocessing_dataset.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import streamlit as st
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
@@ -9,9 +8,12 @@ from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix,mean_squared_error,r2_score
 
-st.title("PREPROCESSING")
 
-st.write("Read File - AppleStore[ About rating]")
+
+def load_data():
+    df = pd.read_csv("ScikitLearn/AppleStore.csv")
+    return df
+
 
 df=pd.read_csv("ScikitLearn/AppleStore.csv")
 df=df.drop(["id","track_name","currency","ver","cont_rating","prime_genre"], axis=1)
@@ -22,103 +24,71 @@ X = df.drop("user_rating", axis=1)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-menu=st.sidebar.radio("Select option",["Main","StandardScaler","SimpleImputer","Metrics"])
+pipeline_concept=Pipeline([
+        ("scaler", StandardScaler()),
+        ("model", LogisticRegression())
+        ])
 
-if menu == "Main":
-    col1,col2,col3=st.columns(3)
-    pipeline_concept=Pipeline([
-                ("scaler", StandardScaler()),
-                ("model", LogisticRegression())
-                ])
-    with col1:
-        st.write("Random Forest Classifier")
-        random_forest=RandomForestClassifier()
-        random_forest.fit(X_train,y_train)
-        C=random_forest.score (X_test,y_test)
-        st.info(C)
+random_forest=RandomForestClassifier()
+random_forest.fit(X_train,y_train)
+C=random_forest.score (X_test,y_test)
+print("RandomForestClassifier",C)
 
-    with col2: 
-        st.write("Logistic Regression")
-        model = LogisticRegression(max_iter=1000)
-        model.fit(X_train,y_train)
-        D = model.predict(X_test)
-        st.info(D)
 
-    with col3:
-        st.write("Pipeline")
-        pipeline_concept.fit(X_train,y_train)
-        E=pipeline_concept.score(X_test,y_test)
-        st.info(E)
 
-    st.subheader("Grid Search")
-    co1,co2=st.columns(2)
-    param_grid={
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train,y_train)
+D = model.predict(X_test)
+print("LogisticRegression",D)
+
+
+
+pipeline_concept.fit(X_train,y_train)
+E=pipeline_concept.score(X_test,y_test)
+print("Pipeline",E)
+
+param_grid={
     "n_estimators": [50,100,200],
     "max_depth": [None, 5,10]}
 
-    grid=GridSearchCV(RandomForestClassifier(), param_grid,cv=5)
-    grid.fit(X_train,y_train)
+grid=GridSearchCV(RandomForestClassifier(), param_grid,cv=5)
+grid.fit(X_train,y_train)
 
-    with co1:
-        st.write("Best parameters")
-        st.info( grid.best_params_)
-    with co2:
-        st.write("Best score:")
-        st.info( grid.best_score_)
 
-    param_grid={
+print("Best parameters")
+print (grid.best_params_)
+
+print("Best score:")
+print( grid.best_score_)
+
+param_grid={
     "model__C": [0.01,0.1,1,10],
     "model__solver":["lbfgs"]
     }
 
-    grid=GridSearchCV(pipeline_concept, param_grid,cv=3)
-    grid.fit(X_train,y_train)
-    best_model=grid.best_estimator_
+grid=GridSearchCV(pipeline_concept, param_grid,cv=3)
+grid.fit(X_train,y_train)
+best_model=grid.best_estimator_
 
-    y_pred=best_model.predict(X_test)
+y_pred=best_model.predict(X_test)
 
-    st.subheader("Best parameters - model__c | model__solver")
-    st.info(grid.best_params_)
+print("Best parameters - model__c | model__solver")
+print(grid.best_params_)
 
-if menu == "StandardScaler":
-    st.header("Standard Scaler")
-    scaler = StandardScaler()
-    X= scaler.fit_transform(X)
-    st.text(X)
+print("Standard Scaler")
+scaler = StandardScaler()
+X= scaler.fit_transform(X)
+print(X)
 
-if menu == "SimpleImputer":
-    st.header("Simple Imputer")
-    imputer = SimpleImputer()
-    X= imputer.fit_transform(X)
-    st.text(X)
+print("Simple Imputer")
+imputer = SimpleImputer()
+X= imputer.fit_transform(X)
+print(X)
 
 
-if menu == "Metrics":
-    st.header("Metrics")
-
-    pipeline_concept=Pipeline([
-                ("scaler", StandardScaler()),
-                ("model", LogisticRegression())
-                ])
-
-    param_grid={
-    "model__C": [0.01,0.1,1,10],
-    "model__solver":["lbfgs"]
-    }
-
-    grid=GridSearchCV(pipeline_concept, param_grid,cv=3)
-    grid.fit(X_train,y_train)
-    best_model=grid.best_estimator_
-
-    y_pred=best_model.predict(X_test)
-    st.write("Accuracy:")
-    st.info(accuracy_score (y_test,y_pred))
-    st.write("Mean Squared Error")
-    st.info( mean_squared_error(y_test,y_pred))
-    st.write("Classification Report")
-    st.info( classification_report(y_test,y_pred))
-    st.write("Confusion Matrix")
-    st.info(confusion_matrix(y_test,y_pred))
-    st.write("r_Square Score")
-    st.info(r2_score(y_test,y_test))
+print("Accuracy:",accuracy_score (y_test,y_pred))
+print("Mean Squared Error", mean_squared_error(y_test,y_pred))
+print("Classification Report", classification_report(y_test,y_pred))
+print("Confusion Matrix",confusion_matrix(y_test,y_pred))
+print("r_Square Score",r2_score(y_test,y_test))
 
---
+++ preprocessing_streamlit.py
@@ -0,0 +1,135 @@
+import pandas as pd
+import streamlit as st
+from preprocessing_dataset import load_data
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix,mean_squared_error,r2_score
+
+
+df = load_data()
+
+st.title("PREPROCESSING")
+
+st.write("Read File - AppleStore[ About rating]")
+
+df=df.drop(["id","track_name","currency","ver","cont_rating","prime_genre"], axis=1)
+y=df["user_rating"]
+
+y = (y >= 4).astype(int)
+X = df.drop("user_rating", axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+menu=st.sidebar.radio("Select option",["Main","Parameters","StandardScaler","SimpleImputer","Metrics"])
+
+if menu == "Main":
+    st.header("Datasets")
+    min_year=int(df["S.No"].min())
+    max_year=int(df["S.No"].max())
+
+    year_range=st.slider("Filter Range",min_year,max_year,(min_year,max_year))
+    filtering_dataset=df[(df["S.No"]>=year_range[0])&(df["S.No"]<=year_range[1])]
+    st.dataframe(filtering_dataset)
+   
+elif menu == "Parameters":
+    col1,col2,col3=st.columns(3)
+    pipeline_concept=Pipeline([
+                ("scaler", StandardScaler()),
+                ("model", LogisticRegression())
+                ])
+    with col1:
+        st.write("Random Forest Classifier")
+        random_forest=RandomForestClassifier()
+        random_forest.fit(X_train,y_train)
+        C=random_forest.score (X_test,y_test)
+        st.info(C)
+
+    with col2: 
+        st.write("Logistic Regression")
+        model = LogisticRegression(max_iter=1000)
+        model.fit(X_train,y_train)
+        D = model.predict(X_test)
+        st.info(D)
+
+    with col3:
+        st.write("Pipeline")
+        pipeline_concept.fit(X_train,y_train)
+        E=pipeline_concept.score(X_test,y_test)
+        st.info(E)
+
+    st.subheader("Grid Search")
+    co1,co2=st.columns(2)
+    param_grid={
+    "n_estimators": [50,100,200],
+    "max_depth": [None, 5,10]}
+
+    grid=GridSearchCV(RandomForestClassifier(), param_grid,cv=5)
+    grid.fit(X_train,y_train)
+
+    with co1:
+        st.write("Best parameters")
+        st.info( grid.best_params_)
+    with co2:
+        st.write("Best score:")
+        st.info( grid.best_score_)
+
+    param_grid={
+    "model__C": [0.01,0.1,1,10],
+    "model__solver":["lbfgs"]
+    }
+
+    grid=GridSearchCV(pipeline_concept, param_grid,cv=3)
+    grid.fit(X_train,y_train)
+    best_model=grid.best_estimator_
+
+    y_pred=best_model.predict(X_test)
+
+    st.subheader("Best parameters - model__c | model__solver")
+    st.info(grid.best_params_)
+
+elif menu == "StandardScaler":
+    st.header("Standard Scaler")
+    scaler = StandardScaler()
+    X= scaler.fit_transform(X)
+    st.write(X)
+
+elif menu == "SimpleImputer":
+    st.header("Simple Imputer")
+    imputer = SimpleImputer()
+    X= imputer.fit_transform(X)
+    st.write(X)
+
+
+elif menu == "Metrics":
+    st.header("Metrics")
+
+    pipeline_concept=Pipeline([
+                ("scaler", StandardScaler()),
+                ("model", LogisticRegression())
+                ])
+
+    param_grid={
+    "model__C": [0.01,0.1,1,10],
+    "model__solver":["lbfgs"]
+    }
+
+    grid=GridSearchCV(pipeline_concept, param_grid,cv=3)
+    grid.fit(X_train,y_train)
+    best_model=grid.best_estimator_
+
+    y_pred=best_model.predict(X_test)
+    st.write("Accuracy:")
+    st.info(accuracy_score (y_test,y_pred))
+    st.write("Mean Squared Error")
+    st.info( mean_squared_error(y_test,y_pred))
+    st.write("Classification Report")
+    st.info( classification_report(y_test,y_pred))
+    st.write("Confusion Matrix")
+    st.info(confusion_matrix(y_test,y_pred))
+    st.write("r_Square Score")
+    st.info(r2_score(y_test,y_test))
